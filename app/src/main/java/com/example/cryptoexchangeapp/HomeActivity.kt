c6ba2fab3888dd3a1aa9142854f122b9e75f791c
Tiger-Catori <michaeladeleye365@gmail.com>
2023-02-01 13:36:23 +0000

    Trying to get google sign in to work

--- app/src/main/java/com/example/cryptoexchangeapp/HomeActivity.kt
+++ app/src/main/java/com/example/cryptoexchangeapp/HomeActivity.kt
@@ -3,6 +3,8 @@ package com.example.cryptoexchangeapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import com.example.cryptoexchangeapp.databinding.ActivityHomeBinding
 import com.google.firebase.auth.FirebaseAuth
@@ -19,13 +21,29 @@ class HomeActivity : AppCompatActivity() {
 
         auth = FirebaseAuth.getInstance()
 
+        val email = intent.getStringExtra("Email")
+        val displayName = intent.getStringExtra("Name")
+
+        // findViewById<TextView>(R.id.textView).text = email + "\n" + displayName
+
         binding.btnLogout.setOnClickListener {
             auth.signOut()
+            startActivity(Intent(this, LoginActivity::class.java))
+            /*
             Intent(this, LoginActivity::class.java).also {
                 it.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
                 startActivity(it)
                 Toast.makeText(this, "Logout Successful", Toast.LENGTH_SHORT).show()
             }
+
+            findViewById<Button>(R.id.btn_logout).setOnClickListener {
+                auth.signOut()
+
+            }
+            */
+
+
         }
+
     }
 }
--- app/src/main/java/com/example/cryptoexchangeapp/LoginActivity.kt
+++ app/src/main/java/com/example/cryptoexchangeapp/LoginActivity.kt
@@ -1,21 +1,41 @@
 package com.example.cryptoexchangeapp
 
 import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.example.cryptoexchangeapp.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.auth.api.signin.internal.GoogleSignInOptionsExtensionParcelable
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.jakewharton.rxbinding2.widget.RxTextView
 import io.reactivex.Observable
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.util.regex.Pattern
+
+const val REQUEST_CODE_SIGN_IN = 0
 
 @SuppressLint("CheckResult")
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
     private lateinit var auth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,7 +45,10 @@ class LoginActivity : AppCompatActivity() {
 // Firebase Auth
         auth = FirebaseAuth.getInstance()
 
+
+
 // Username Validation
+
         val usernameStream = RxTextView.textChanges(binding.etEmail)
             .skipInitialValue()
             .map { username ->
@@ -34,6 +57,7 @@ class LoginActivity : AppCompatActivity() {
         usernameStream.subscribe {
             showTextMinimalAlert(it, "Email/Username")
         }
+
 
 // Password Validation
         val passwordStream = RxTextView.textChanges(binding.etPassword)
@@ -61,22 +85,81 @@ class LoginActivity : AppCompatActivity() {
             }
         }
 
-// Click startActivity(Intent(this, HomeActivity::class.java))
+// Login Button
         binding.btnLogin.setOnClickListener{
             val email = binding.etEmail.text.toString().trim()
             val password = binding.etPassword.text.toString().trim()
             loginUser(email, password)
         }
 
+// Login With Google Button
+        binding.btnLoginGoogle.setOnClickListener {
+            val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.webclient_id))
+                .requestEmail()
+                //.requestProfile()
+                .build()
+            googleSignInClient = GoogleSignIn.getClient(this, gso)
+            //   findViewById<Button>(R.id.btn_login_google).setOnClickListener {
+                signInGoogle()
+            // }
+        }
+
+
+
+
+// No account Register Button
         binding.tvHaventAccount.setOnClickListener{
             startActivity(Intent(this, RegisterActivity::class.java))
         }
 
+// Forgotten Password Button
         binding.tvForgotPw.setOnClickListener {
             startActivity(Intent(this, ResetPasswordActivity::class.java))
         }
     }
 
+      private fun signInGoogle() {
+          val signInIntent = googleSignInClient.signInIntent
+          launcher.launch(signInIntent)
+      }
+
+      private val launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+          result ->
+                    if (result.resultCode == Activity.RESULT_OK) {
+                        val task = GoogleSignIn.getSignedInAccountFromIntent(result.data)
+                        handleResults(task)
+                    }
+      }
+
+    private fun handleResults(task: Task<GoogleSignInAccount>?) {
+        if (task != null) {
+            if (task.isSuccessful) {
+                val account: GoogleSignInAccount? = task.result
+                if (account != null) {
+                    updateUI(account)
+                }
+            } else {
+                Toast.makeText(this, task.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun updateUI(account: GoogleSignInAccount) {
+        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
+        auth.signInWithCredential(credentials).addOnCompleteListener {
+            if (it.isSuccessful) {
+                val intent: Intent = Intent(this, HomeActivity::class.java)
+                /*
+                intent.putExtra("Email", account.email)
+                intent.putExtra("Name", account.displayName)
+                */
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 
     private fun showTextMinimalAlert(isNotValid: Boolean, text: String) {
         if (text == "Email/Username")
